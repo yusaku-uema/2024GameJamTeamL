6687defca2038e375966bff2817af1fced7257dc
yasutokuniyosi <138412658+yasutokuniyosi@users.noreply.github.com>
2024-02-13 12:00:37 +0900

    プレイヤー2　移動

--- TeamL/TeamL/Player2.cpp
+++ TeamL/TeamL/Player2.cpp
@@ -1,20 +1,66 @@
 #include "Player2.h";
 #include "DxLib.h";
+#include "PadInput.h"
 
 #define PLAYER2_SIZE_X   (30.0f)
 
 Player2::Player2()
 {
-	x = 100.0f;
-	y = 100.0f;
+	x = 1200.0f;
+	y = 350.0f;
+	move_y = 1.0f;
+	radius = 10.0f;
+}
+
+Player2::Player2(float mx, float my)
+{
+	x = mx;
+	y = my;
+	move_y = 5.0f;
+	radius = 10.0f;
 }
 
 void Player2::Update()
 {
+	y += move_y;
 
+	if (y > (480 - radius) || y < (0 + radius))
+	{
+		move_y *= -1.0f;
+	}
 }
 
 void Player2::Draw()
 {
-	DrawBoxAA(x - PLAYER2_SIZE_X, y, x + PLAYER2_SIZE_X, y + 10.0f, GetColor(255, 255, 255),TRUE);
+	DrawCircleAA(x , y, radius, 100, GetColor(255, 255, 255),TRUE);
+}
+
+void Player2::Move()
+{
+	//キーを使って動かす
+	if (CheckHitKey(KEY_INPUT_UP) != FALSE) // 上キーを押すと上に行く
+	{
+		y -= 5.0f;
+	}
+	else if (CheckHitKeyAll() == 0)//キーを離すと止まる
+	{
+		y += 0.0f;
+	}
+
+	if (CheckHitKey(KEY_INPUT_DOWN) != FALSE) //下キーを押すと下に行く
+	{
+		y += 5.0f;
+	}
+	else if (CheckHitKeyAll() == 0)//キーを離すと止まる
+	{
+		y += 0.0f;
+	}
+
+	//同時に上下ボタン押すと止まる
+	if (CheckHitKey(KEY_INPUT_UP) != FALSE && CheckHitKey(KEY_INPUT_DOWN) != FALSE)
+	{
+		y += 0.0f;
+	}
+
+	
 }
--- TeamL/TeamL/Player2.h
+++ TeamL/TeamL/Player2.h
@@ -1,15 +1,19 @@
+//š ‹g@Nm‚ª’S“–‚µ‚½‚Æ‚±‚ë
+
 #pragma once
 class Player2
 {
 private:
 	float x;
 	float y;
+	float move_y;
+	float radius;
 
 public:
 	Player2();
+	Player2(float mx, float my);
 
 	void Update();
 	void Draw();
-
-	void SetLocation(float mx, float my);
+	void Move();
 };
