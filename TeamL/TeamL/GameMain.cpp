2573a261fcdcd4da5a01165f4851fde9a5d1a5e4
manato0115 <manato-nakama-k22f@stu.kbc.ac.jp>
2024-02-13 09:34:29 +0900

    あ

--- TeamL/TeamL/GameMain.cpp
+++ TeamL/TeamL/GameMain.cpp
@@ -15,7 +15,10 @@ GameMain::~GameMain()
 AbstractScene* GameMain::Update()
 {
 
+
 	return this;
+
+
 }
 
 void GameMain::Draw()const
---
+++ TeamL/TeamL/camerawork.cpp
@@ -0,0 +1,34 @@
+#include"DxLib.h"
+#include"camerawark.h"
+
+
+CameraWork::CameraWork()
+{
+
+
+
+}
+CameraWork::~CameraWork()
+{
+
+
+
+}
+
+
+AbstractScene* CameraWork::Update()
+{
+
+
+	return this;
+
+
+}
+
+void CameraWork::Draw()const
+{
+
+
+
+
+}
